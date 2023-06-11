98eea66009314522b5e5f4469c4bf39540df9cd3
Aleksandr <bestprogreraxe@gmail.com>
2023-06-11 20:26:42 +0300

    Solved task 28, final commit

--- Task_28/main.py
+++ Task_28/main.py
@@ -6,3 +6,18 @@
 2 2
     4
 """
+
+def sum(a, b):
+    if b > 0:
+        b -= 1
+        a += 1
+        return sum(a, b)
+    return print(f'Sum of these numbers is {a}.')
+
+
+number_a = int(input('Enter first integer non-negative number (a): '))
+number_b = int(input('Enter second integer non-negative number (b): '))
+if number_a >= 0 and number_b >= 0:
+    sum_of_numbs = sum(number_a, number_b)
+else:
+    print('You entered negative number, try again.')
